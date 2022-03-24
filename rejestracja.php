49e0308d783dad6ad12ccd794a9413bd484b4a45
Krzysztof Kiemel <101550837+krisskiemel@users.noreply.github.com>
2022-03-24 13:49:53 +0100

    rejestracja1

--- rejestracja.php
+++ rejestracja.php
@@ -17,30 +17,43 @@
     </head>    
         
     <body>
-        <?php include "menu.php";?>
-        <div class="container-fluid">
-            <form action="rejestracja.php" method="get">
-                <div class="mb-3 mt-3">
-                    <label for="nazwisko" class="form-label">Imię nazwisko:</label>
-                    <input type="text" class="form-control" id="nazwisko" name="nazwisko" required>
-                </div>
-                <div class="mb-3 mt-3">
-                    <label for="login" class="form-label">Login:</label>
-                    <input type="text" class="form-control" id="login" name="login" required>
-               </div>
-               <div class="mb-3 mt-3">
-                    <label for="password" class="form-label">Hasło:</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
-               </div>
-               <div class="mb-3 mt-3">
-                    <label for="password2" class="form-label">Powtórz hasło:</label>
-                    <input type="password" class="form-control" id="password2" name="password2" required>
-               </div>
-                <div class="mb-3 mt-3">
-                    <button type="submit" class="btn btn-<?php echo $kolory?>">Zarejestruj</button>
-                </div>
-            </form>
-        </div>
+        <?php include "menu.php";
+        if (!isset('typ')) {?>
+            <div class="container-fluid">
+                <form action="rejestracja.php" method="get">
+                    <input type="hidden" name="typ" value="insert">
+                    <div class="mb-3 mt-3">
+                        <label for="nazwisko" class="form-label">Imię Nazwisko:</label>
+                        <input type="text" class="form-control" id="nazwisko" name="nazwisko" required>
+                    </div>
+                    <div class="mb-3 mt-3">
+                        <label for="login" class="form-label">Login:</label>
+                        <input type="text" class="form-control" id="login" name="login" required>
+                    </div>
+                    <div class="mb-3 mt-3">
+                            <label for="password" class="form-label">Hasło:</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="mb-3 mt-3">
+                            <label for="password2" class="form-label">Powtórz hasło:</label>
+                            <input type="password" class="form-control" id="password2" name="password2" required>
+                    </div>
+                    <div class="mb-3 mt-3">
+                        <button type="submit" class="btn btn-<?php echo $kolory?>">Zarejestruj</button>
+                    </div>
+                </form>
+            </div>
+        <?php
+        } else {
+            $nazwisko = $_REQUEST['nazwisko'];
+            $login = $_REQUEST['login'];
+            $password = md5($_REQUEST['password']);
+            $password2 = md5($_REQUEST['password2']);
+            if ($password == $password2) {
+                
+            }
+
+        }    
 
     </body>
 
